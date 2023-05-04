32c9b301e17b52a74dc2168640d6678f8f58ecb5
Ahmad Saeed <ahmad.sw7788@gmail.com>
2023-05-04 23:54:14 +0500

    task-3

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+   protected $table = 'menu_items';
+   protected $fillable = ['name', 'url', 'parent_id'];
+   public $timestamps = ['created_at', 'updated_at'];
 
+   public function children()
+   {
+      return $this->hasMany(Self::class, 'parent_id', 'id');
+   }
 }
